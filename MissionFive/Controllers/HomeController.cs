1ed0a6a3686247a481ac79524b36fa03770ff2c2
Seth Alley <salley32@byu.edu>
2024-02-07 14:45:11 -0700

    Add project files.

---
+++ MissionFive/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MissionFive.Models;
+using System.Diagnostics;
+
+namespace MissionFive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult pagetwo()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MissionFive/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+$(document).ready(function () {
+    $("#btnSubmit").click(function () {
+        let hours = parseFloat($("#hours").val());
+        let rate = parseFloat($("#rate").val());
+        let total = hours * rate;
+
+        let outputDiv = $("#output");
+        outputDiv.empty(); // Clear previous content
+
+        let resultText = $("<p>").text("Total Price: $" + total.toFixed(2));
+        outputDiv.append(resultText);
+    });
+});
